f8cc40f31c3de262ea17146ad37264eee07bf680
unknown <>
2017-03-22 14:05:20 +0800

    --new pro

---
+++ app/src/main/java/com/example/firstrepository/act/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.firstrepository.act;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 张泽雅文 on 2017/3/22.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
